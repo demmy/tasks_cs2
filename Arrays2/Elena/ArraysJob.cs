aafb4fa912c4c42822033a387cd44f52025563b7
YaVanilko <ya.vanilko@gmail.com>
2015-08-17 00:42:46 +0300

    Homework

--- Arrays2/Elena/ArraysJob.cs
+++ Arrays2/Elena/ArraysJob.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,7 @@ namespace Arrays2.Elena
 
                     }
                     thereIsLine = true;
+
                 }
             }
 
